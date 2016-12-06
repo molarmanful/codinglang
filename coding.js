281c7a03d3629ea12d451be99096e0dc01bd4c63
Ben Pang <benjinpang@gmail.com>
2016-12-05 17:15:35 -0800

    change execution

--- coding.js
+++ coding.js
@@ -1,4 +1,5 @@
 Coding={}
+
 Coding.parser=peg.generate(String.raw`
 start=a:(_(str/mod)_)*{
   return a.map(x=>x[1])
@@ -13,37 +14,48 @@ str='"'a:([^"\\]/'\\'.)*'"'?{
   }
 }
 
-mod=tag/class/id/attr
-tag='>'h:[A-Za-z]t:[-_A-Za-z0-9]*{
+mod=tag/class/id/attr/op
+tag='>'h:$([A-Za-z][-_A-Za-z0-9]*){
   return{
     type:'tag',
-    body:[h].concat(t).join('')
+    body:h
   }
 }
-class='.'h:"-"?t:[_a-zA-Z]+T:[_a-zA-Z0-9-]*{
+class='.'h:$("-"?[_a-zA-Z]+[_a-zA-Z0-9-]*){
   return{
     type:'class',
-    body:[h||''].concat(t,T).join('')
+    body:h
   }
 }
-id='#'h:[A-Za-z]t:[-_A-Za-z0-9]*{
+id='#'h:$([A-Za-z][-_A-Za-z0-9]*){
   return{
     type:'id',
-    body:[h].concat(t).join('')
+    body:h
   }
 }
-attr='@'h:[A-Za-z]t:[-_A-Za-z0-9]*{
+attr='@'h:$([A-Za-z][-_A-Za-z0-9]*){
   return{
     type:'attr',
-    body:[h].concat(t).join('')
+    body:h
   }
 }
-`,{})
+op=h:$(.+){
+  return{
+    type:'op',
+    body:h
+  }
+}
+`)
+
 Coding.eval=(x,y)=>{
-  let code=Coding.parser.parse(x)
+  let code=Coding.parser.parse(x.replace(/^[ \n]*/g,''))
   let strst=[]
   let elest=[]
-  code.map(a=>{
+  let base={
+    '$s':_=>strst.push(strst[strst.length-1])
+  }
+
+  for(ip=0;a=code[ip];ip++){
     if(a.type=='str'){
       strst.push(a.body)
     }
@@ -61,6 +73,10 @@ Coding.eval=(x,y)=>{
     if(a.type=='attr'){
       elest[elest.length-1].setAttribute(a.body,strst.pop())
     }
-  })
-  y(elest,strst)
+    if(a.type=='op'){
+      base[a.body]()
+    }
+  }
+
+  y(strst,elest)
 }
