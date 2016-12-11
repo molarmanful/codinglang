995e460f63be5d1a360411ba5fb621ac38a07e7f
Ben Pang <benjinpang@gmail.com>
2016-12-11 12:26:54 -0800

    change language's direction

--- coding.js
+++ coding.js
@@ -14,7 +14,7 @@ num=a:$[0-9]+{
     return +a
 }
 
-mod=tag/class/id/attr/op
+mod=fn/tag/class/id/attr/op
 tag='>'h:$([A-Za-z][-_A-Za-z0-9]*){
   return{
     type:'tag',
@@ -39,16 +39,22 @@ attr='@'h:$([A-Za-z][-_A-Za-z0-9]*){
     body:h
   }
 }
-op=h:$([^ \n]+){
+op=h:$([^ \n()""@#.]+){
   return{
     type:'op',
     body:h
   }
 }
+fn='('a:start')'b:op{
+  return{
+    type:'fn',
+    body:a,
+    f:b.body
+  }
+}
 `)
 
 Coding.eval=(x,y)=>{
-  let code=Coding.parser.parse(x.replace(/^[ \n]*/g,''))
   let sst=[]
   let est=[]
   let base={
@@ -58,35 +64,45 @@ Coding.eval=(x,y)=>{
     '%e':_=>est.splice(est.length-est.pop()-1,1),
     '\\s':_=>sst.splice(sst.length-sst.pop()-1,0,sst.pop()),
     '\\e':_=>est.splice(est.length-est.pop()-1,0,est.pop()),
-    '+':_=>sst.push(sst.pop()+sst.pop()),
-    '-':_=>sst.push(-sst.pop()- -sst.pop()),
-    '*':_=>sst.push(sst.pop()*sst.pop()),
-    '/':(x=sst.pop(),y=sst.pop())=>sst.push(0|y/x,y%x),
-    '^':(x=sst.pop(),y=sst.pop())=>sst.push(Math.pow(y,x)),
+    '>':_=>est.push(sst.pop()||''),
+    '<h':_=>sst.push(est.pop().innerHTML),
+    '<t':_=>sst.push(est.pop().innerText),
+    '+s':_=>sst[sst.length-2]+=sst.pop(),
+    '+e':_=>
+      est[est.length-2].innerHTML!=[]._?
+        (est[est.length-2].innerHTML+=est.pop())
+      :est[est.length-1].innerHTML!=[]._?
+        (est[est.length-1].innerHTML=est.splice(-2,1)+est[est.length-1].innerHTML)
+      :(est[est.length-2]+=est.pop())
   }
-
-  for(let ip=0,a;a=code[ip],a||a==''||a==0;ip++){
-    if(!a.type){
-      sst.push(a)
-    }
-    else if(a.type=='tag'){
-      let el=document.createElement(a.body)
-      el.innerHTML=sst.pop()||''
-      est.push(el)
-    }
-    else if(a.type=='class'){
-      est[est.length-1].class+=' '+a.body
-    }
-    else if(a.type=='id'){
-      est[est.length-1].id=a.body
-    }
-    else if(a.type=='attr'){
-      est[est.length-1].setAttribute(a.body,sst.pop())
-    }
-    else if(a.type=='op'){
-      base[a.body]()
+  let E=x=>{
+    for(let ip=0,a;a=x[ip],a||a==''||a==0;ip++){
+      if(!a.type){
+        sst.push(a)
+      }
+      else if(a.type=='tag'){
+        let el=document.createElement(a.body)
+        el.innerHTML=sst.pop()||''
+        est.push(el)
+      }
+      else if(a.type=='class'){
+        est[est.length-1].class+=' '+a.body
+      }
+      else if(a.type=='id'){
+        est[est.length-1].id=a.body
+      }
+      else if(a.type=='attr'){
+        est[est.length-1].setAttribute(a.body,sst.pop())
+      }
+      else if(a.type=='op'){
+        base[a.body]()
+      }
+      else if(a.type=='fn'){
+        base[a.f]=_=>E(a.body)
+      }
     }
   }
+  E(Coding.parser.parse(x.replace(/^[ \n]*/g,'')))
 
-  y(sst,est)
+  return[sst,est]
 }
